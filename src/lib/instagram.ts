68ecd2d11c7d7ded24808582c32e268337573ae0
Түвшинсайхан <tuvshee894@gmail.com>
2026-03-13 11:00:53 +0800

    fixed the instagram.ts file problem i think we are good

--- src/lib/instagram.ts
+++ src/lib/instagram.ts
@@ -1,16 +1,10 @@
 import fetch from "node-fetch";
 
-// const IG_TOKEN = process.env.INSTAGRAM_PAGE_ACCESS_TOKEN;
 const IG_TOKEN = process.env.FACEBOOK_PAGE_ACCESS_TOKEN;
 
 function requireToken() {
-  if (!IG_TOKEN) throw new Error("INSTAGRAM_PAGE_ACCESS_TOKEN not set");
+  if (!IG_TOKEN) throw new Error("FACEBOOK_PAGE_ACCESS_TOKEN not set");
   return IG_TOKEN;
-}
-
-function requireIgUserId(igUserId?: string | null) {
-  if (!igUserId) throw new Error("INSTAGRAM_BUSINESS_ID not available");
-  return igUserId;
 }
 
 export async function sendTextMessage(
@@ -19,9 +13,9 @@ export async function sendTextMessage(
   text: string,
 ) {
   const token = requireToken();
-  const id = requireIgUserId(igUserId);
+
   const res = await fetch(
-    `https://graph.facebook.com/v16.0/${id}/messages?access_token=${token}`,
+    `https://graph.facebook.com/v19.0/me/messages?access_token=${token}`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
